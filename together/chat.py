c6731b062fe8ce3b7c8c03731dce4826eeba1e7e
lazyslacker <nimisht@gmail.com>
2025-01-31 23:48:44 +0000

    more messing around

--- together/chat.py
+++ together/chat.py
@@ -6,10 +6,29 @@ load_dotenv()
 
 TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
 
+#client = Together()
+
+#response = client.chat.completions.create(
+#    model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
+#    messages=[{"role": "user", "content": "What are some fun things to do in New York?"}],
+#)
+#print(response.choices[0].message.content)
+
 client = Together()
+
+message = [{'role': 'user', 'content': 'Whats the date and time?'}]
 
 response = client.chat.completions.create(
     model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
-    messages=[{"role": "user", "content": "What are some fun things to do in New York?"}],
+    messages=message,
+    max_tokens=None,
+    temperature=0.7,
+    top_p=0.7,
+    top_k=50,
+    repetition_penalty=1,
+    stop=["<｜end▁of▁sentence｜>"],
+    stream=True
 )
-print(response.choices[0].message.content)
+for token in response:
+    if hasattr(token, 'choices'):
+        print(token.choices[0].delta.content, end='', flush=True)
